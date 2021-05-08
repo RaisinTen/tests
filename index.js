e6ebbeb36e84fc6f6e3826438b0786eeb13d638f
Darshan Sen <raisinten@gmail.com>
2021-05-08 19:12:40 +0530

    devnull test

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log(process.env);
+const fs = require('fs');
+
+const devnull = process.platform === 'win32' ? '\\\\.\\nul' : '/dev/null';
+console.log(`devnull: "${devnull}"`);
+
+const fd = fs.openSync(devnull, 'r+');
+console.log(fd);
+const stats = fs.fstatSync(fd);
+console.log(stats);
+
+fs.closeSync(fd);
