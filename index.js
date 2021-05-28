30208f3d17ca8de9eb70212fa8822bf715fcc8e2
Darshan Sen <raisinten@gmail.com>
2021-05-28 20:04:28 +0530

    check getuid on Windows

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const fs = require('fs');
-
-const devnull = process.platform === 'win32' ? '\\\\.\\nul' : '/dev/null';
-console.log(`devnull: "${devnull}"`);
-
-console.log(fs.existsSync(devnull));
-
-console.log(fs.accessSync(devnull));
-
-const stats = fs.statSync(devnull);
-console.log(stats);
+console.log(process.getuid());
