4f00c31fdb47eb025225f1a53f98656c895c1ca1
Darshan Sen <raisinten@gmail.com>
2021-05-31 13:58:38 +0530

    test readdirSync accessibility

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-const os = require('os');
+const fs = require('fs');
+const path = require('path');
 
-console.log(os.platform());
+const cwdpath = process.cwd();
+const dirpath = path.join(cwdpath, 'dir');
+const filepath = path.join(dirpath, 'file');
+
+fs.mkdirSync(dirpath);
+fs.writeFileSync(filepath, 'hello');
+
+fs.chmodSync(filepath, 0o444);
+fs.chmodSync(dirpath, 0o444);
+
+let err = null;
+
+try {
+  console.log(fs.readdirSync(dirpath));
+} catch (_err) {
+  err = _err;
+}
+
+try {
+  fs.chmodSync(dirpath, 0o777);
+} catch {
+}
+
+try {
+  fs.chmodSync(filepath, 0o777);
+} catch {
+}
+
+try {
+  fs.rmSync(dirpath, { recursive: true, force: true });
+} catch {
+}
+
+if (err !== null) {
+  throw err;
+}
