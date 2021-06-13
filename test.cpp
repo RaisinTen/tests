1739dab5c09c2f287e87b6a9b943442cfc3ffda1
Darshan Sen <raisinten@gmail.com>
2021-06-13 20:34:29 +0530

    use `std::setlocale()`

--- test.cpp
+++ test.cpp
@@ -4,7 +4,12 @@
 int main() {
   std::cout << "Initial locale: \"" << std::locale().name() << "\"" << std::endl;
 
-  std::locale::global(std::locale(""));
+  std::string currentLocale = std::setlocale(LC_ALL, nullptr);
+  std::string systemLocale = std::setlocale(LC_ALL, "");
+
+  std::setlocale(LC_ALL, currentLocale.c_str());
+
+  std::locale::global(std::locale(systemLocale));
 
   std::cout << "System locale: \"" << std::locale().name() << "\"" << std::endl;
 
