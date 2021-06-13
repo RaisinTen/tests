e026b5e5c8f5f17d81416b021992d169d23d277b
Darshan Sen <raisinten@gmail.com>
2021-06-13 21:00:15 +0530

    add GetLocaleCountryCode

--- test.cpp
+++ test.cpp
@@ -1,5 +1,20 @@
 #include <locale>
 #include <iostream>
+#include <Windows.h>
+
+void GetLocaleCountryCode() {
+  WCHAR locale_name[LOCALE_NAME_MAX_LENGTH] = {0};
+
+  if (GetLocaleInfoEx(LOCALE_NAME_USER_DEFAULT, LOCALE_SISO3166CTRYNAME,
+        (LPWSTR)&locale_name,
+        sizeof(locale_name) / sizeof(WCHAR)) ||
+      GetLocaleInfoEx(LOCALE_NAME_SYSTEM_DEFAULT, LOCALE_SISO3166CTRYNAME,
+        (LPWSTR)&locale_name,
+        sizeof(locale_name) / sizeof(WCHAR))) {
+  }
+
+  wprintf(L"%s\n", ws);
+}
 
 int main() {
   std::cout << "Initial locale: \"" << std::locale().name() << "\"" << std::endl;
@@ -12,6 +27,8 @@ int main() {
   std::locale::global(std::locale(systemLocale));
 
   std::cout << "System locale: \"" << std::locale().name() << "\"" << std::endl;
+
+  GetLocaleCountryCode();
 
   return 0;
 }
