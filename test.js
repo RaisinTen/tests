3abee9e7a2344e450eaa1715dd89718a31abde66
Darshan Sen <darshan.sen@postman.com>
2021-10-31 21:09:23 +0530

    chmod 0o777 too

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+
 console.log(fs.statSync('C:\\Windows'));
 fs.chmodSync('C:\\Windows', 0o000);
+console.log(fs.statSync('C:\\Windows'));
+
+console.log(fs.statSync('C:\\Windows'));
+fs.chmodSync('C:\\Windows', 0o777);
 console.log(fs.statSync('C:\\Windows'));
