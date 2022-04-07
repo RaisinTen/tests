7a08038063b24ab0e079f182be04ba20f04063a3
Darshan Sen <raisinten@gmail.com>
2022-04-07 14:57:11 +0530

    Do crypto after app is ready
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 const { app, safeStorage } = require("electron");
 
-console.log(safeStorage.isEncryptionAvailable());
-console.log(safeStorage.encryptString("hello"));
-
-app.quit();
+(async () => {
+  await app.whenReady();
+  console.log(safeStorage.isEncryptionAvailable());
+  console.log(safeStorage.encryptString("hello"));
+})()
+  .then(app.quit)
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
