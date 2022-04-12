0e261657ccf21fab1b0206e7db1a9d9663058ddf
Darshan Sen <raisinten@gmail.com>
2022-04-12 15:19:30 +0530

    Remove BrowserWindow
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

--- test.js
+++ test.js
@@ -1,12 +1,11 @@
-const { app, BrowserWindow, safeStorage } = require('electron');
+const { app, safeStorage } = require('electron');
 const { setTimeout: setTimeoutPromises } = require('timers/promises');
 
 (async () => {
   await app.whenReady();
-  new BrowserWindow();
   await setTimeoutPromises(10000);
-  // console.log(safeStorage.isEncryptionAvailable());
-  // console.log(safeStorage.encryptString("hello"));
+  console.log(safeStorage.isEncryptionAvailable());
+  console.log(safeStorage.encryptString("hello"));
 })()
   .then(app.quit)
   .catch((err) => {
